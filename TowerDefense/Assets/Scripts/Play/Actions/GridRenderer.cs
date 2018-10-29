2e973b42ab8fafb1d97723b942e2abdaa64c547b
PieterWinderlichSchool <pieterwinderlich@outlook.com>
2018-10-29 15:10:45 +0100

    add's scripts

---
+++ TowerDefense/Assets/Scripts/Play/Actions/GridRenderer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridRenderer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Grid myGrid = new Grid(20, 20);
+		print (myGrid.GetTile (10, 10).IsBlocking);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
